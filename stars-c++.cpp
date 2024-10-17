f415f961cf7dd4844325f82d35ef225602f720c5
matvey144 <matveybb@yandex.ru>
2024-10-17 11:33:15 +0300

    Cleaned up stars c++.

--- stars-c++.cpp
+++ stars-c++.cpp
@@ -1,21 +1,19 @@
 #include "raylib.h"
 #include <cmath>
-#include <cstdlib>
-#include <ctime>
+#include <random>
 
-#define MAX_STARS 64
-#define RADIUS 5.0
+constexpr int maxStars = 64;
+constexpr float radius = 5.0;
+constexpr float minSpeed = 1.0;
+constexpr float maxSpeed = 10.0;
 
-/* stars-c++.cpp - basically a port of stars2d.c to C++.                      *
- * I thought using of class function would result in more readable code.      */
-
-// I know rand() is not recomended in C++, but I wanted to port stars2d.c 
-// with minimal changes to functionality.
-float generateRandomFloat(float minimum, float maximum)
-{
-	float scale = std::rand() / (float) RAND_MAX;
-	return minimum + scale * (maximum - minimum);
-}
+//TODO: remake using 2dCamera
+//Initializing random number generator for class Star
+std::random_device randDevice;
+std::mt19937 rng(randDevice());
+std::uniform_real_distribution<float> randomAngle(0, 2 * M_PI);
+std::uniform_real_distribution<float> randomSpeed(minSpeed, maxSpeed);
+std::uniform_int_distribution<unsigned char> randomColor(0, 0xFF);
 
 class Star {
 public:
@@ -29,14 +27,15 @@ public:
 		coords.x += xOffset + std::cos(angle) * speed;
 		coords.y += yOffset + std::sin(angle) * speed;
 	}
-	void resetPosition(int startX, int startY)
+	Star(int startX, int startY)
 	{
-		speed = generateRandomFloat(1.0, 10.0);
-		angle = generateRandomFloat(0.0, M_PI * 2);
+		speed = randomSpeed(rng);
+		angle = randomAngle(rng);
 		coords = (Vector2) {(float) startX, (float) startY};
-		int temp = std::rand();
-		unsigned char *byte_ptr = (unsigned char *) &temp;
-		color = (Color) {byte_ptr[0], byte_ptr[1], byte_ptr[2], 0xFF};
+		color.r = randomColor(rng);
+		color.g = randomColor(rng);
+		color.b = randomColor(rng);
+		color.a = 0xFF;
 	}
 };
 
@@ -50,21 +49,23 @@ int main(void)
 	const int minHeight = 240;
 	int halfHeight = defaultHeight / 2;
 	int halfWidth = defaultWidth / 2;
+
 	bool noColor = false; 
 
-	std::srand(time(NULL));
-	Star stars[MAX_STARS];
+	Star *stars = (Star *) malloc(maxStars * sizeof (Star));
+	for (int i = 0; i < maxStars; i++)
 	
 	stars[0].angle = 0.0;
 	stars[0].speed = 0.0;
 	stars[0].coords = (Vector2) { (float)halfWidth, (float)halfHeight };
 	stars[0].color = WHITE;
-	for (int i = 1; i < MAX_STARS; i++) {
-		stars[i].resetPosition(halfWidth, halfHeight);
+	for (int i = 1; i < maxStars; i++) {
+		stars[i] = Star(halfWidth, halfHeight);
 	}
 
 	SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT);
-	InitWindow(defaultWidth, defaultHeight, u8"stars-2d in C++. Press spacebar to change color mode.");
+	InitWindow(defaultWidth, defaultHeight,
+		u8"stars-2d in C++. Press spacebar to change color mode.");
 	SetWindowMinSize(minWidth, minHeight);
 	SetTargetFPS(60);
 	while (!WindowShouldClose()) {
@@ -76,29 +77,37 @@ int main(void)
 		// Needed to offset depending on changed window size
 		int halfDiffWidth = (curWidth - prevWidth) / 2;
 		int halfDiffHeight = (curHeight - prevHeight) / 2;
+
 		// For checking if star is still within window.
-		Rectangle screenRec = (Rectangle) {0, 0, (float) curWidth, (float) curHeight};
+		Rectangle screenRec;
+		screenRec.x = 0.0;
+		screenRec.y = 0.0;
+		screenRec.width = (float) curWidth;
+		screenRec.height = (float) curHeight;
+
 		// Switching color mode.
 		if (IsKeyReleased(KEY_SPACE)) noColor = !noColor;
+
 		// Don't like this hack, but without it central star is slightly off-center after resizing window.
-		stars[0].resetPosition(halfWidth, halfHeight);
-		for (int i = 1; i < MAX_STARS; i++) {
+		stars[0] = Star (halfWidth, halfHeight);
+
+		for (int i = 1; i < maxStars; i++) {
 			stars[i].moveStar(halfDiffWidth, halfDiffHeight);
 			if (!CheckCollisionPointRec(stars[i].coords, screenRec))
-				stars[i].resetPosition(halfWidth, halfHeight);
+				stars[i] = Star(halfWidth, halfHeight);
 		}
 		BeginDrawing();
 		{
 			ClearBackground(BLACK);
 			if (noColor) {
-				for(int i = 0; i < MAX_STARS; i++)
-					DrawCircleV(stars[i].coords, RADIUS, WHITE);
+				for(int i = 0; i < maxStars; i++)
+					DrawCircleV(stars[i].coords, radius, WHITE);
 			}
 			else {
-				for(int i = 1; i < MAX_STARS; i++) {
-					DrawCircleV(stars[i].coords, RADIUS, stars[i].color);
+				for(int i = 1; i < maxStars; i++) {
+					DrawCircleV(stars[i].coords, radius, stars[i].color);
 				}
-				DrawCircleV(stars[0].coords, RADIUS, WHITE);
+				DrawCircleV(stars[0].coords, radius, WHITE);
 			}
 #ifdef DEBUG
 			DrawFPS(0, 0);
