d11ab2d48299118637a0be6831608803c72d5c31
matvey144 <matveybb@yandex.ru>
2024-10-17 10:09:26 +0300

    Cleaned up star-balls.cpp and added option to change color mode.

--- star-balls.cpp
+++ star-balls.cpp
@@ -1,20 +1,35 @@
 #include "raylib.h"
 #include <random>
 
+constexpr float maxXY = 15.0;
+constexpr float minXY = -15.0;
+constexpr float maxSpeed = 3.0;
+constexpr float minSpeed = 0.1;
+constexpr float maxZ = 65.0;
+constexpr float minZ = 50.0;
+
+//Initializing RNG.
+std::random_device randDevice;
+std::mt19937 rng(randDevice());
+std::uniform_real_distribution<float> distrXY(minXY, maxXY);
+std::uniform_int_distribution<unsigned char> distrColor(0, 255);
+std::uniform_real_distribution<float> distrSpeed(minSpeed, maxSpeed);
+std::uniform_real_distribution<float> distrZ(minZ, maxZ);
+
 #define MAX_STARS 30
 
 class StarBall {
 public:
-	float radius;
+	static constexpr float radius = 1.0;
 	Vector3 position;
 	float speed;
+	Color color;
 
-	//TODO: make a more readable initialization function;
-	void resetStar(float newX, float newY, float newZ, float newSpeed)
+	StarBall()
 	{
-		radius = 1.0;
-		position = (Vector3) {newX, newY, newZ};
-		speed = newSpeed;
+		position = (Vector3) {distrXY(rng), distrXY(rng), distrZ(rng)};
+		speed = distrSpeed(rng);
+		color = (Color) {distrColor(rng), distrColor(rng), distrColor(rng), 0xFF};
 	}
 
 	void moveStar()
@@ -25,25 +40,10 @@ public:
 
 int main()
 {
-	const float maxXY = 15.0;
-	const float minXY = -15.0;
-	const float maxSpeed = 3.0;
-        const float minSpeed = 0.1;
-	const float maxZ = 65.0;
-	const float minZ = 50.0;
 	const float cameraPos = -10.0;
-
-	//Initializing RNG.
-	std::random_device randDevice;
-	std::mt19937 rng(randDevice());
-	std::uniform_real_distribution<> distrXY(minXY, maxXY);
-	std::uniform_real_distribution<> distrSpeed(minSpeed, maxSpeed);
-	std::uniform_real_distribution<> distrZ(minZ, maxZ);
+	bool noColor = false;
 
 	StarBall stars[MAX_STARS];
-	for(int i = 0; i < MAX_STARS; i++) {
-		stars[i].resetStar(distrXY(rng), distrXY(rng), distrZ(rng), distrSpeed(rng));
-	}
 
 	Camera3D camera;
 	camera.position = (Vector3) {0.0, 0.0, cameraPos};
@@ -53,23 +53,31 @@ int main()
 	camera.projection = CAMERA_PERSPECTIVE;
 
 	SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT);
-	InitWindow(640, 480, u8"3d Balls.");
+	InitWindow(640, 480, u8"3d Balls. Press spacebar to change color mode.");
 	SetWindowMinSize(320, 240);
 	SetTargetFPS(60);
 	while(!WindowShouldClose()) {
+		if (IsKeyReleased(KEY_SPACE)) noColor = !noColor;
 		for (int i = 0; i < MAX_STARS; i++)
 		{
 			stars[i].moveStar();
 			if (stars[i].position.z <= cameraPos) {
-				stars[i].resetStar(distrXY(rng), distrXY(rng), distrZ(rng), distrSpeed(rng));
+				stars[i] = StarBall();
 			}
 		}
 		BeginDrawing();
 		ClearBackground(BLACK);
 		BeginMode3D(camera);
 		{
-			for (int i = 0; i < MAX_STARS; i++) {
-				DrawSphere(stars[i].position, stars[i].radius, WHITE);
+			if (noColor) {
+				for (int i = 0; i < MAX_STARS; i++) {
+					DrawSphere(stars[i].position, stars[i].radius, WHITE);
+				}
+			}
+			else {
+				for (int i = 0; i < MAX_STARS; i++) {
+					DrawSphere(stars[i].position, stars[i].radius, stars[i].color);
+				}
 			}
 		}
 		EndMode3D();
