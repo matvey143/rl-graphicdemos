2f0bd8a4110957799bc3b408448061d27a560f68
matvey144 <matveybb@yandex.ru>
2024-10-29 10:37:31 +0300

    Started trying to make oldZunLogo resizable

--- oldZunLogo.c
+++ oldZunLogo.c
@@ -11,12 +11,6 @@
 #define MIN_Y -30.0f
 #define MAX_Y  60.0f
 
-float randomFloat(float minimum, float maximum)
-{
-	float scale = rand() / (float) RAND_MAX;
-	return minimum + scale * (maximum - minimum);
-}
-
 struct bgStar {
 	Vector2 coords;
 	float speed;
@@ -29,6 +23,13 @@ struct IntroBall {
 	float velY;
 };
 
+float randomFloat(float minimum, float maximum)
+{
+	float scale = rand() / (float) RAND_MAX;
+	return minimum + scale * (maximum - minimum);
+}
+
+
 void resetStar(struct bgStar *star)
 {
 	star->coords.x = randomFloat(MIN_X, MAX_X);
@@ -38,14 +39,16 @@ void resetStar(struct bgStar *star)
 	star->speed = randomFloat(minSpeed, maxSpeed);
 }
 
-void moveStar(struct bgStar *star, float angle)
+void moveStar(struct bgStar *star, float angle,
+		float widthScale, float heightScale)
 {
 	star->coords.x += cos(angle) * star->speed;
 	star->coords.y += sin(angle) * star->speed;
 
+	static const float height = MAX_X - MIN_X, width = MAX_Y - MIN_Y;
 	// Checking if star is out of bounds
-	static const Rectangle collisionBox = {MIN_X, MIN_Y,
-		MAX_X - MIN_X, MAX_Y - MIN_Y};
+	Rectangle collisionBox = {MIN_X * widthScale, MIN_Y * heightScale,
+		width * widthScale, height * heightScale};
 	if (!CheckCollisionPointRec(star->coords, collisionBox))
 		resetStar(star);
 }
@@ -53,35 +56,33 @@ void moveStar(struct bgStar *star, float angle)
 //TODO: Needs agjustments.
 // Thanks to github user cash-i1 I figured out how to handle wall hits.
 // github.com/cash-i1/raylib-dvd-logo 
-void moveBall(struct IntroBall *ball_ptr)
+void moveBall(struct IntroBall *ball_ptr, float widthScale, float heightScale)
 {
 	ball_ptr->coords.x += ball_ptr->velX;
 	ball_ptr->coords.y += ball_ptr->velY;
-	if (ball_ptr->coords.x >= MAX_X) ball_ptr->velX = -1.0;
-	if (ball_ptr->coords.x <= MIN_X) ball_ptr->velX = 1.0;
-	if (ball_ptr->coords.y >= MAX_Y) ball_ptr->velY = -1.0;
-	if (ball_ptr->coords.y <= MIN_Y) ball_ptr->velY = 1.0;
+	if (ball_ptr->coords.x >= MAX_X * widthScale) ball_ptr->velX = -1.0;
+	if (ball_ptr->coords.x <= MIN_X * widthScale) ball_ptr->velX = 1.0;
+	if (ball_ptr->coords.y >= MAX_Y * heightScale) ball_ptr->velY = -1.0;
+	if (ball_ptr->coords.y <= MIN_Y * heightScale) ball_ptr->velY = 1.0;
 }
 
+// TODO: make window resizable
 // I wanted to recreate old ZUN intro (Touhou 1-3).
 int main(void)
 {
 	const float radius = 20.0;
-	const int ballAmount = 4;
+	const int tempBallAmount = 3;
 
-	struct IntroBall balls[ballAmount];
-	for (int i = 0; i < ballAmount; i++) {
-		balls[i].coords = (Vector2) {-6.0, 6.0};
-		balls[i].velX = -1.0;
-		balls[i].velY = 1.0;
-		//Need to move balls apart.
-		for(int j = 0; j < 10 * i; j++) moveBall(&balls[i]);
-	}
-	// These colors came from picking colors from screenshot.
-	balls[0].color = (Color) {0x00, 0x00, 0x63, 0xFF};
-	balls[1].color = (Color) {0x00, 0x00, 0x9C, 0xFF};
-	balls[2].color = (Color) {0x00, 0x00, 0xEF, 0xFF};
-	balls[3].color = (Color) {0x63, 0x75, 0xFF, 0xFF};
+	struct IntroBall mainBall;
+	struct IntroBall tempBalls[ballAmount];
+
+	// TODO: there are long term desync between tempBalls if window is resized.
+	// Need to adjust program to address this behavour. 
+	// These colors came from picking colors of screenshot, might be inaccurate.
+	mainBall.color = (Color) {0x00, 0x00, 0x63, 0xFF};
+	tempBalls[0].color = (Color) {0x00, 0x00, 0x9C, 0xFF};
+	tempBalls[1].color = (Color) {0x00, 0x00, 0xEF, 0xFF};
+	tempBalls[2].color = (Color) {0x63, 0x75, 0xFF, 0xFF};
 
 	// Initiating stars in background.
 	const int starAmount = 12;
@@ -97,9 +98,13 @@ int main(void)
 	camera.zoom = 3.0;
 
 	// Might adjust program to make window resizable.
-	const int screenWidth = 640, screenHeight = 480;
-	InitWindow(screenWidth, screenHeight, u8"Old ZUN logo.");
-	RenderTexture camTexture = LoadRenderTexture(screenWidth,screenHeight/2);
+	const int defaultWidth = 640, defaultHeight = 480;
+	int curWidth = defaultWidth, curHeight = defaultHeight;
+	int prevWidth = 0, prevHeight = 0;
+	SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT);
+	InitWindow(defaultWidth, defaultHeight, u8"Old ZUN logo.");
+	SetWindowMinSize(320, 240);
+	RenderTexture camTexture;
 
 	// Text and fonts;
 	char touhou[] = u8"\xe6\x9d\xb1\xe6\x96\xb9 project";
@@ -108,9 +113,22 @@ int main(void)
 	
 	SetTargetFPS(60);
 	while (!WindowShouldClose()) {
-		for (int i = 0; i < starAmount; i++) moveStar(&stars[i], angle);
-		for (int i = 0; i < ballAmount; i++) moveBall(&balls[i]);
+		curWidth = GetScreenWidth();
+		curHeight = GetScreenHeight();
+		float widthScale = (float) curWidth / defaultWidth;
+		float heightScale = (float) curHeight / defaultHeight;
+		// Window is resized.
+		if (curWidth != prevWidth && curHeight != prevHeight) {
+			UnloadRenderTexture(camTexture);
+			camTexture = LoadRenderTexture(curWidth, curHeight/2);
+		}
+
+		for (int i = 0; i < starAmount; i++)
+			moveStar(&stars[i], angle, widthScale, heightScale);
+		for (int i = 0; i < ballAmount; i++)
+			moveBall(&tempBalls[i], widthScale, heightScale);
 		angle += 0.01;
+		// Basically a reset every full turn.
 		if (angle > PI + PI) angle -= PI + PI;
 
 		BeginTextureMode(camTexture);
@@ -121,7 +139,7 @@ int main(void)
 				DrawPixelV(stars[i].coords, WHITE);
 			}
 			for(int i = 0; i < ballAmount; i++) {
-				DrawCircleV(balls[i].coords, radius, balls[i].color);
+				DrawCircleV(tempBalls[i].coords, radius, tempBalls[i].color);
 			}
 			EndMode2D();
 		}
@@ -129,14 +147,16 @@ int main(void)
 		BeginDrawing();
 		{
 			ClearBackground(BLACK);
-			DrawTexture(camTexture.texture, 0, screenHeight/4,
+			DrawTexture(camTexture.texture, 0, curHeight/4,
 					(Color) { 255, 255, 255, 255});
 			DrawTextEx(mainfont, touhou,
-				(Vector2) {(screenWidth - textSize.x) / 2,
-				(screenHeight - textSize.y) / 2},
+				(Vector2) {(curWidth - textSize.x) / 2,
+				(curHeight - textSize.y) / 2},
 				32.0, 0.0, WHITE);
 		}
 		EndDrawing();
+		prevWidth = curWidth;
+		prevHeight = curHeight;
 	}
 	CloseWindow();
 	return 0;
