de93d9b2cac774fe0cf09ff7e848909ee71fb1af
matvey144 <matveybb@yandex.ru>
2024-11-26 13:25:31 +0300

    Continued working on friend.c

--- friend.c
+++ friend.c
@@ -1,17 +1,31 @@
+#include <math.h>
 #include "raylib.h"
 
 // TESTING DO NOT USE.
+// Might not work on tiling WM without support of transperency
 int main(void)
 {
-	SetConfigFlags(FLAG_WINDOW_MOUSE_PASSTHROUGH | FLAG_WINDOW_TRANSPARENT | FLAG_WINDOW_UNDECORATED);
+	SetConfigFlags(FLAG_WINDOW_MOUSE_PASSTHROUGH | 
+			// I struggle to get it working on XFCE
+			FLAG_WINDOW_TRANSPARENT |
+			FLAG_WINDOW_UNDECORATED |
+			FLAG_WINDOW_TOPMOST |
+			FLAG_WINDOW_UNFOCUSED);
 	InitWindow(GetScreenWidth(), GetScreenHeight(), "Friend");
 	SetWindowPosition(0, 0);
+	Vector2 FriendXY = {400.0f, 400.0f};
 	while (!WindowShouldClose())
 	{
+		if (IsKeyDown(KEY_C) & IsKeyDown(KEY_RIGHT_CONTROL) |
+				IsKeyDown(KEY_C) & IsKeyDown(KEY_LEFT_CONTROL)){
+			CloseWindow();
+			return 0;
+		}
+		// TODO: Add here logic to move friend according to cursor XY.
 		BeginDrawing();
 		{
 			ClearBackground(BLANK);
-			DrawCircle(400, 400, 20, YELLOW);
+			DrawCircleV(FriendXY, 20.0f, YELLOW);
 		}
 		EndDrawing();
 	}
