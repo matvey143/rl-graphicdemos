feedf0cc535cd1aa97bdb22bc8ea77e9887d25d0
matvey144 <matveybb@yandex.ru>
2025-01-28 09:48:35 +0300

    Added TODOs

--- current-time.c
+++ current-time.c
@@ -3,6 +3,7 @@
 
 int main(void)
 {
+	// I intend to make digits size change depending on window size.
 	SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT);
 	InitWindow(640, 480, u8"Clock.");
 	SetTargetFPS(60);
@@ -10,6 +11,7 @@ int main(void)
 		BeginDrawing();
 		{
 			ClearBackground(BLACK);
+			// TODO: Here should be "non-lit" clock digits.
 		}
 		EndDrawing();
 	}
