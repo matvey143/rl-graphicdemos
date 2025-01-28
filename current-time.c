cde0941612e428ac9ded1f234c4f0968c1353ebb
matvey144 <matveybb@yandex.ru>
2025-01-28 12:46:29 +0300

    WIP current-time.c

--- current-time.c
+++ current-time.c
@@ -1,6 +1,14 @@
 #include "raylib.h"
 #include <time.h>
 
+int drawDigit(int offset, int ledLength, int ledWidth)
+{
+	//Top horizontal
+	DrawRectangle(ledWidth + ledWidth + offset, ledWidth, ledLength, ledWidth, GRAY);
+	offset += 3 * ledWidth + ledLength;
+	return offset;
+}
+
 int main(void)
 {
 	// I intend to make digits size change depending on window size.
@@ -8,9 +16,17 @@ int main(void)
 	InitWindow(640, 480, u8"Clock.");
 	SetTargetFPS(60);
 	while (!WindowShouldClose()) {
+		// TODO: finish. Horizontal and vertical LEDs
+		// should have different length.
+		int width = GetScreenWidth();
+		int height = GetScreenHeight();
+		int ledWidth = width / (23 * 5);
+		int hLedLength = (width - ledWidth * 23) / 6;
+
 		BeginDrawing();
 		{
 			ClearBackground(BLACK);
+			drawDigit(0, ledLength, ledWidth);
 			// TODO: Here should be "non-lit" clock digits.
 		}
 		EndDrawing();
