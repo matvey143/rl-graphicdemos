5fb3bd0393d694d9e0d672975f54e8498e02a605
matvey144 <matveybb@yandex.ru>
2025-01-30 18:42:35 +0300

    All digits are in place.

--- current-time.c
+++ current-time.c
@@ -1,6 +1,14 @@
 #include "raylib.h"
 #include <time.h>
 
+int drawSeparator(int offset, int ledWidth, int screenheight)
+{
+	DrawRectangle(offset + ledWidth, screenheight / 4, ledWidth, ledWidth, GREEN);
+	DrawRectangle(offset + ledWidth, screenheight / 4 + screenheight / 2, ledWidth, ledWidth, GREEN);
+	offset += 2 * ledWidth;
+	return offset;
+}
+
 int drawDigit(int offset, int hLedLength, int vLedLength, int ledWidth)
 {
 	// Top horizontal
@@ -28,8 +36,7 @@ int main(void)
 	InitWindow(640, 360, u8"Clock.");
 	SetTargetFPS(60);
 	while (!WindowShouldClose()) {
-		// TODO: finish. Horizontal and vertical LEDs
-		// should have different length.
+		// Might not be perfect.
 		int screenwidth = GetScreenWidth();
 		int screenheight = GetScreenHeight();
 		int ledWidth = screenwidth / (23 * 5);
@@ -39,9 +46,17 @@ int main(void)
 		BeginDrawing();
 		{
 			ClearBackground(BLACK);
+			// Hours.
 			int offset = drawDigit(0, hLedLength, vLedLength, ledWidth);
 			offset = drawDigit(offset, hLedLength, vLedLength, ledWidth);
-			// TODO: Here should be "non-lit" clock digits.
+			offset = drawSeparator(offset, ledWidth, screenheight);
+			// Minutes.
+			offset = drawDigit(offset, hLedLength, vLedLength, ledWidth);
+			offset = drawDigit(offset, hLedLength, vLedLength, ledWidth);
+			offset = drawSeparator(offset, ledWidth, screenheight);
+			// Seconds.
+			offset = drawDigit(offset, hLedLength, vLedLength, ledWidth);
+			offset = drawDigit(offset, hLedLength, vLedLength, ledWidth);
 		}
 		EndDrawing();
 	}
